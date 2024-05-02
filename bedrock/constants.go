fcf97eb960ced441840967a5357a6183052a3cf9
haitran <hai@entest.io>
2024-05-02 01:53:26 +0000

    update constants.go

--- bedrock/constants.go
+++ bedrock/constants.go
@@ -11,4 +11,4 @@ const KNOWLEDGE_BASE_ID = "X3CHIODXQZ"
 const KNOWLEDGE_BASE_MODEL_ID = "anthropic.claude-3-haiku-20240307-v1:0"
 const KNOWLEDGE_BASE_NUMBER_OF_RESULT = 6
 const AOSS_ENDPOINT = "https://yvp6plo4ijurgy8ymhdg.us-east-1.aoss.amazonaws.com"
-const AOSS_NOTE_APP_INDEX_NAME = "note"
+const AOSS_NOTE_APP_INDEX_NAME = "demo"
