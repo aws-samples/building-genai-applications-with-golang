aacb83084ede7d16e4cde87acf5a01f0d2c0a495
Hai Tran <htranminhhai20@gmail.com>
2025-09-02 06:28:38 +0700

    Update bedrock.go

--- bedrock/bedrock.go
+++ bedrock/bedrock.go
@@ -55,8 +55,6 @@ type Response struct {
 	Completion string `json:"completion"`
 }
 
-type HelloHandler struct{}
-
 type Query struct {
 	Topic string `json:"topic"`
 }
@@ -125,99 +123,6 @@ func HandleBedrockClaude3HaikuChat(w http.ResponseWriter, r *http.Request, Bedro
 				f.Flush()
 			} else {
 				fmt.Println("Damn, no flush")
-			}
-
-		case *types.UnknownUnionMember:
-			fmt.Println("unknown tag:", v.Tag)
-
-		default:
-			fmt.Println("union is nil or unknown type")
-		}
-	}
-}
-
-func HandleHaikuImageAnalyzer(w http.ResponseWriter, r *http.Request, BedrockClient *bedrockruntime.Client) {
-
-	// data type request
-	type Message struct {
-		Role    string        `json:"role"`
-		Content []interface{} `json:"content"`
-	}
-
-	type Request struct {
-		Messages []Message `json:"messages"`
-	}
-
-	type RequestBodyClaude3 struct {
-		MaxTokensToSample int       `json:"max_tokens"`
-		Temperature       float64   `json:"temperature,omitempty"`
-		AnthropicVersion  string    `json:"anthropic_version"`
-		Messages          []Message `json:"messages"`
-	}
-
-	// parse request
-	var request Request
-	error := json.NewDecoder(r.Body).Decode(&request)
-
-	if error != nil {
-		panic(error)
-	}
-
-	// payload for bedrock claude3 haikue
-	messages := request.Messages
-
-	payload := RequestBodyClaude3{
-		MaxTokensToSample: 2048,
-		AnthropicVersion:  "bedrock-2023-05-31",
-		Temperature:       0.9,
-		Messages:          messages,
-	}
-
-	// convert payload struct to bytes
-	payloadBytes, error := json.Marshal(payload)
-
-	if error != nil {
-		fmt.Println(error)
-	}
-
-	// invoke bedrock claude3 haiku
-	output, error := BedrockClient.InvokeModelWithResponseStream(
-		context.Background(),
-		&bedrockruntime.InvokeModelWithResponseStreamInput{
-			Body:        payloadBytes,
-			ModelId:     aws.String("anthropic.claude-3-haiku-20240307-v1:0"),
-			ContentType: aws.String("application/json"),
-			Accept:      aws.String("application/json"),
-		},
-	)
-
-	if error != nil {
-		fmt.Println(error)
-	}
-
-	// stream result to client
-	for event := range output.GetStream().Events() {
-
-		switch v := event.(type) {
-		case *types.ResponseStreamMemberChunk:
-
-			var resp ResponseClaude3
-			err := json.NewDecoder(bytes.NewReader(v.Value.Bytes)).Decode(&resp)
-			if err != nil {
-				fmt.Println(err)
-			}
-
-			// stream to client
-			// fmt.Println(resp.Delta.Text)
-			var tpl = template.Must(template.New("tpl").Parse(resp.Delta.Text))
-			tpl.Execute(w, nil)
-			// another way and client parse it
-			// json.NewEncoder(w).Encode(resp)
-
-			if f, ok := w.(http.Flusher); ok {
-				f.Flush()
-			} else {
-				fmt.Print("Damn, no flush")
 			}
 
 		case *types.UnknownUnionMember:
