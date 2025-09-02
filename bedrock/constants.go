bb7a3f39250dc65a2920f552ce41845e54fae813
Hai Tran <htranminhhai20@gmail.com>
2025-09-02 15:01:45 +0700

    Update constants.go

--- bedrock/constants.go
+++ bedrock/constants.go
@@ -7,7 +7,9 @@ package bedrock
 const BEDROCK_REGION = "us-west-2"
 const AOSS_REGION = "us-west-2"
 const KNOWLEDGE_BASE_REGION = "us-west-2"
+# update this knowledge base id 
 const KNOWLEDGE_BASE_ID = "NN0ZVCX8YS"
+# update this knowledge base model id 
 const KNOWLEDGE_BASE_MODEL_ID = "anthropic.claude-3-haiku-20240307-v1:0"
 const KNOWLEDGE_BASE_NUMBER_OF_RESULT = 6
 const AOSS_ENDPOINT = "https://yvp6plo4ijurgy8ymhdg.us-east-1.aoss.amazonaws.com"
