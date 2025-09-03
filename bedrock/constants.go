04e34e25ad558a16f38de83eafa086efde93b610
Hai Tran <htranminhhai20@gmail.com>
2025-09-03 07:41:22 +0700

    Update constants.go

--- bedrock/constants.go
+++ bedrock/constants.go
@@ -12,4 +12,4 @@ const KNOWLEDGE_BASE_MODEL_ID = "anthropic.claude-3-haiku-20240307-v1:0"
 const KNOWLEDGE_BASE_NUMBER_OF_RESULT = 6
 const AOSS_ENDPOINT = "https://yvp6plo4ijurgy8ymhdg.us-east-1.aoss.amazonaws.com"
 const AOSS_NOTE_APP_INDEX_NAME = "demo"
-const MODEL_ID = "anthropic.claude-3-haiku-20240307-v1:0"
+const MODEL_ID = "anthropic.claude-3-5-haiku-20241022-v1:0"
